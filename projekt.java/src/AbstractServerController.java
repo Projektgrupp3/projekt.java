5cf901ae498a61e0ba409f1f70ffbc279b0ad661
David Buö <davidbauwie@gmail.com>
2011-10-24 11:58:12 +0200

    allt

--- projekt.java/src/AbstractServerController.java
+++ projekt.java/src/AbstractServerController.java
@@ -4,31 +4,45 @@ import java.io.IOException;
 
 public abstract class AbstractServerController {
 	private BufferedReader in;
-	private boolean listen;
+	private boolean listen = true;
 	private MultiServerView view;
-
+	private MultiServerThread mst;
+	
 	public AbstractServerController(BufferedReader in){
 		this.in = in;
 	}
-	
+
 	public AbstractServerController(BufferedReader in, MultiServerView msv){
 		this.in = in;
 		this.view = msv;
 	}
+	public AbstractServerController(BufferedReader in, MultiServerThread mst){
+		this.in = in;
+		this.mst = mst;
+	}
 
-	public void listen() throws IOException{
+	public void listen() throws IOException {
+		try{
 		while(listen){
 			String input;
-			if((input = in.readLine()) != null){
+			System.out.println("Lyssnar");
+			if((input = in.readLine()) != ""){
+				if(input.equals(null)){
+					listen = false;
+				}
 				System.out.println("Input from Client: "+input);
 				evaluate(input);
 			}
 		}
+		} catch(NullPointerException e){
+			listen = false;
+		}
 	}
 
 	public void evaluate(String input){
-		String inputSplit[] = input.split("_", 4);
-		view.send(inputSplit[0]+" "+inputSplit[1]);
+		//String inputSplit[] = input.split("_", 4);
+		//view.send(inputSplit[0]+" "+inputSplit[1]);
+		view.send(input);
 	}
 
 	public boolean isListen() {
