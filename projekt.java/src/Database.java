5fc5d933e0b12a6093e364e41473fc048615e6c7
Emil Bergwik <bergwik@gmail.com>
2011-10-24 15:35:33 +0200

    En ändring till i databasen.

--- projekt.java/src/Database.java
+++ projekt.java/src/Database.java
@@ -2,6 +2,6 @@ public class Database {
 	public static void main(String[] args){
 		System.out.println("kaoz123");
 		System.out.println("Hej David");
+		System.out.println("En ändring till.");
 	}
-
 }
