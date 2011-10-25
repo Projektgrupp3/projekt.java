40872da98bcfe44f909b510d4562c9c2adfd1f9d
David Buö <davidbauwie@gmail.com>
2011-10-25 10:18:18 +0200

    o

--- projekt.java/src/TestClient.java
+++ projekt.java/src/TestClient.java
@@ -14,6 +14,8 @@ public class TestClient {
 	private static PrintWriter out;
 	private static BufferedReader reader;
 	private static String username="user_", password="pass_";
+	private static final String COM_IP = "130.236.226.59";
+	private static final int COM_PORT = 4444;
 
 	/**
 	 * @param args
@@ -24,26 +26,23 @@ public class TestClient {
 	}
 
 	public static void createServerConnection() throws UnknownHostException, IOException{
-		String ip = "130.236.226.59";
-		ip = "130.236.227.111";
-		int port = 4444;
-		client = new Socket(ip,port);
+		client = new Socket(COM_IP,COM_PORT);
 
 		in = new InputStreamReader(client.getInputStream());
 		out = new PrintWriter(client.getOutputStream(),true);
 		reader = new BufferedReader(in);
 
-		System.out.println("Connected to Server @ "+ip+":"+port);
+		System.out.println("Connected to Server @ "+COM_IP+":"+COM_PORT);
 	}
 
 	public static void waitForInput() throws IOException{
 		while(true){
 			System.out.println("Input your username:");
 			Scanner in = new Scanner(System.in);
-			username.concat(in.nextLine());
+			username +=in.nextLine();
 			send(username);
 			System.out.println("Input your password:");
-			password.concat(in.nextLine());
+			password += in.nextLine();
 			send(password);		
 			System.out.println("Sending user authentication..");
 			String serverInput;	
